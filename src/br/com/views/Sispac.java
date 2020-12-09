776495cc20fb4d46a7be1fc06bc7d87abefb323a
Victor Gomes <victorcfalx5@hotmail.com>
2020-12-09 20:53:38 -0300

    pdf

--- src/br/com/views/Sispac.java
+++ src/br/com/views/Sispac.java
@@ -1,11 +1,14 @@
 package br.com.views;
-
+import br.com.pdf.*;
 import br.com.migrates.*;
 import br.com.editar.*;
+
+import java.sql.SQLException;
+
 import br.com.criar.*;
 
 public class Sispac {
-	public static void main(String[] ags) {
+	public static void main(String[] ags) throws SQLException {
 		//MigrateUP migrates = new MigrateUP();// cria as tabelas no banco 
 		//MigrateDown migrates = new MigrateDown();// remove as tabelas do banco
 
@@ -13,9 +16,10 @@ public class Sispac {
 		//Criar_evento ev = new Criar_evento();
 		//Criar_aluno al = new Criar_aluno(1,"00547847646","joao","Ljosos@gmail","a23re");
 		//Editar_admin ed1 = new Editar_admin();
-		Editar_aluno ed2 = new Editar_aluno();
+		//Editar_aluno ed2 = new Editar_aluno();
 		//Criar_evento ev = new Criar_evento();
 		//Criar_subevento sub = new Criar_subevento();
+		Gerar g = new Gerar(2);
 
 	}
 
