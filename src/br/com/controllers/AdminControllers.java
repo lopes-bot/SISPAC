083a1741533f4df36b4992132e728e1310bacdca
Victor Gomes <victorcfalx5@hotmail.com>
2020-12-09 22:21:08 -0300

    fd

--- src/br/com/controllers/AdminControllers.java
+++
@@ -1,7 +0,0 @@
-package br.com.controllers;
-import br.com.views.*;
-import br.com.criar.*;
-
-public class AdminControllers implements Admin {
-	Criar_admin ad = new Criar_admin(1,"789456123","senha","nome","email@gmail.com");
-}
--- src/br/com/views/Admin.java
+++
@@ -1,5 +0,0 @@
-package br.com.views;
-
-public interface Admin {
-
-}
